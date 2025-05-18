fc19bcbe45805af0740ef3da19bfd937625e0001
nithingollapudi <127386053+nithingollapudi@users.noreply.github.com>
2025-05-18 12:35:19 +0530

    Add files via upload

---
+++ Task 1 _Perform Basic Mathematical Operations.py
@@ -0,0 +1,14 @@
+# task 1: perform basic mathematical operations
+# take input from user
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+# perform operations
+addition = a+b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+# Results
+print("addition: ",addition)
+print("subtraction: ",subtraction)
+print("multiplication: ",multiplication)
+print("division: ",division)
---
+++ Task 2_Greetings.py
@@ -0,0 +1,8 @@
+#Task2:Create a personalized greetings
+#Take name input
+first_name=input("enter your first name: ")
+last_name=input("enter your last name: ")
+#concatenate names
+full_name=first_name + " " + last_name
+#print greeting
+print(f"hello,{full_name}! Welcome to the python course.")
